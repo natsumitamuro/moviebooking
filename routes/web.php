f3163ec9907863f887fe7af7490166912674bcea
tomomi <zdffdycaq@gmail.com>
2022-02-26 13:50:36 +0900

    完了画面を作成しました

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/detail', function () {
-    return view('/detail_screen');
+    return view('detail_screen');
 });
 
+Route::get('/done', function () {
+    return view('done');
+});
