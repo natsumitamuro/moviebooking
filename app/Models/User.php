6c1b14991ca76940b1083cdceaa3637ec79f7a31
ShoContain <perblue.666@gmail.com>
2022-03-13 15:50:56 +0900

    fix: ログイン処理のエラー修正

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory;
 
