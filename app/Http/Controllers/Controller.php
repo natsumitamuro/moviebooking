0336c05ab4ae3970cd9da65aad3d60fcac4d976e
YumaJikei <kuu324@gmail.com>
2022-04-02 15:02:52 +0900

    コメントアウト削除

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,46 +14,9 @@ use Illuminate\Support\Facades\DB;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
+    //映画ごとの予約数をカウント
     public function ranking()
     {
-        //$result=Movie::all()->scheduledMovies()->reservations()->groupBy('id')->count();
-        //dd($result);
-
-        /*$max = Movie::count();
-        $ranking1 = [0,'null'];
-        $ranking2 = [0,'null'];
-        $ranking3 = [0,'null'];
-        
-        for($i=1;$i<=$max;$i++){
-            $ranking = [
-                Reservation::with(['scheduled_movie' => function ($query) {
-                    $query->where('movie_id','=',$i);
-                }])->count(),
-                Movie::find($i)->name,
-            ];
-            
-
-            if($ranking[0] > $ranking1[0]){
-                $ranking3 = $ranking2;
-                $ranking2 = $ranking1;
-                $ranking1 = $ranking;
-            }
-            elseif($ranking[0] > $ranking2[0]){
-                $ranking3 = $ranking2;
-                $ranking2 = $ranking;
-            }
-            elseif($ranking[0] > $ranking3[0]){
-                $ranking3 = $ranking;
-            }
-        }
-        */
-
-        /*$reservtions = Movie::with(['scheduledMovies.reservations'=> function ($query) {
-            $query->groupBy('id');
-            $query->count();
-        }])->get();
-        */
 
         $groups = DB::table('movies')  //＄groupsは変数にいれてるだけ
              ->leftJoin('scheduled_movies', 'movies.id', '=', 'scheduled_movies.movie_id')
@@ -63,7 +26,6 @@ class Controller extends BaseController
              ->orderByDesc('count')
              ->limit(3)
              ->get();
-
 
         return [$groups[0],$groups[1],$groups[2]];
     }
